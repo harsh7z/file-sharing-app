44857abaf41b6291282df56de99ffe7fdf8959bd
harsh7z <harshapatel112003@gmail.com>
2025-07-28 11:57:02 -0500

    ready to run

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -61,7 +61,7 @@ const App = () => {
     });
 
     try {
-      const res = await fetch("http://52.201.253.190:8000/upload/", {
+      const res = await fetch("https://52.201.253.190:8000/upload/", {
         method: "POST",
         body: formData,
       });
